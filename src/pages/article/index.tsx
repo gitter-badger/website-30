ad3901730cbdebb3a8ac8c0d655b3e5fc8844b43
xujiaxin <xujiaxin@kcsj.onaliyun.com>
2020-10-19 16:51:25 +0800

    删除gitment

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,48 +1,10 @@
 import React, { useEffect } from 'react';
 import styles from './index.less';
 import Header from '@/components/header';
-import 'gitment/style/default.css';
-import Gitment from 'gitment';
 interface Props {}
 
 const Index: React.FC<Props> = props => {
-  useEffect(() => {
-    const myTheme = {
-      render(state, instance) {
-        const container = document.createElement('div');
-        container.lang = 'en-US';
-        container.className = 'gitment-container gitment-root-container';
-
-        // your custom component
-        container.appendChild(instance.renderSomething(state, instance));
-
-        container.appendChild(instance.renderHeader(state, instance));
-        container.appendChild(instance.renderEditor(state, instance));
-        container.appendChild(instance.renderComments(state, instance));
-        container.appendChild(instance.renderFooter(state, instance));
-        return container;
-      },
-      renderSomething(state, instance) {
-        const container = document.createElement('div');
-        container.lang = 'en-US';
-        if (state.user.login) {
-          container.innerText = `Hello, ${state.user.login}`;
-        }
-        return container;
-      },
-    };
-    const gitment = new Gitment({
-      owner: 'katteXu',
-      repo: 'comment',
-      oauth: {
-        client_id: 'f935013f0e2e5d101795',
-        client_secret: 'bdacad25920b3fa874344df6b27c711ca2f615fe',
-      },
-      myTheme,
-    });
-
-    gitment.render('comment');
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={styles.main}>
