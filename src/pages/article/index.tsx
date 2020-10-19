afb44d7d56f5228c7da3448ed45227b46a214003
xu <417560450@qq.com>
2020-10-19 17:51:38 +0800

    评论添加

--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -6,33 +6,24 @@ import GitalkComponent from 'gitalk/dist/gitalk-component';
 interface Props {}
 
 const Index: React.FC<Props> = props => {
-  useEffect(() => {
-    // var gitalk = new Gitalk({
-    //   clientID: 'f935013f0e2e5d101795',
-    //   clientSecret: 'bdacad25920b3fa874344df6b27c711ca2f615fe',
-    //   repo: 'comment',
-    //   owner: 'katteXu',
-    //   admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
-    //   id: location.pathname,      // Ensure uniqueness and length less than 50
-    //   distractionFreeMode: false  // Facebook-like distraction free mode
-    // })
-    // gitalk.render('gitalk-container')
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={styles.main}>
       <Header />
       <div>文章标题:title 文章内容:content 文章日期:2020-12-22 19:30</div>
-      <GitalkComponent
-        options={{
-          clientID: 'f935013f0e2e5d101795',
-          clientSecret: 'bdacad25920b3fa874344df6b27c711ca2f615fe',
-          repo: 'comment',
-          owner: 'katteXu',
-          admin: ['katteXu'],
-          language: 'zh-CN',
-        }}
-      ></GitalkComponent>
+      <div className={styles.article}>
+        <GitalkComponent
+          options={{
+            clientID: 'f935013f0e2e5d101795',
+            clientSecret: 'bdacad25920b3fa874344df6b27c711ca2f615fe',
+            repo: 'website',
+            owner: 'katteXu',
+            admin: ['katteXu'],
+            language: 'zh-CN',
+          }}
+        />
+      </div>
     </div>
   );
 };
