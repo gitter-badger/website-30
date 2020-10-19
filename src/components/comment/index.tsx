842629da6393dce73ea4894582405c42b6cc0bef
xu <417560450@qq.com>
2020-10-19 20:49:13 +0800

    评论页

--- src/components/comment/index.tsx
+++ src/components/comment/index.tsx
@@ -1,9 +1,31 @@
 import React, { useEffect } from 'react';
+import 'gitalk/dist/gitalk.css';
+import { dynamic } from 'umi';
+
 interface Props {}
 
+const Comment: any = dynamic({
+  loader: async () => {
+    const { default: GitalkComponent } = await import(
+      'gitalk/dist/gitalk-component'
+    );
+    return GitalkComponent;
+  },
+});
+
 const Index: React.FC<Props> = props => {
-  useEffect(() => {}, []);
-  return <div id="comment_valine"></div>;
+  return (
+    <Comment
+      options={{
+        clientID: 'f935013f0e2e5d101795',
+        clientSecret: 'bdacad25920b3fa874344df6b27c711ca2f615fe',
+        repo: 'website',
+        owner: 'katteXu',
+        admin: ['katteXu'],
+        language: 'zh-CN',
+      }}
+    />
+  );
 };
 
 export default Index;
--- src/pages/article/index.tsx
+++ src/pages/article/index.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import styles from './index.less';
 import Header from '@/components/header';
-import 'gitalk/dist/gitalk.css';
-import GitalkComponent from 'gitalk/dist/gitalk-component';
+import Comment from '@/components/comment';
 interface Props {}
 
 const Index: React.FC<Props> = props => {
@@ -11,18 +10,14 @@ const Index: React.FC<Props> = props => {
   return (
     <div className={styles.main}>
       <Header />
-      <div>文章标题:title 文章内容:content 文章日期:2020-12-22 19:30</div>
       <div className={styles.article}>
-        <GitalkComponent
-          options={{
-            clientID: 'f935013f0e2e5d101795',
-            clientSecret: 'bdacad25920b3fa874344df6b27c711ca2f615fe',
-            repo: 'website',
-            owner: 'katteXu',
-            admin: ['katteXu'],
-            language: 'zh-CN',
-          }}
-        />
+        <p>
+          文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面
+          文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面,文章页面
+        </p>
+      </div>
+      <div className={styles.comment}>
+        <Comment />
       </div>
     </div>
   );
